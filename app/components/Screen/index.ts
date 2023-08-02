6f09ac57d3cb116568608aaf15c6a3c98358cee4
Faithful Chris <75838035+Heyfruz@users.noreply.github.com>
2023-08-02 12:31:30 +0100

    Loan & Savings Screen

--- app/components/Screen/index.ts
+++ app/components/Screen/index.ts
@@ -9,3 +9,4 @@ export { default as Footer } from './Footer';
 export { default as AltHeader } from './AltHeader';
 export { default as SheetContainer } from './SheetContainer';
 export { default as SwirlBackground } from './SwirlBackground';
+export { default as TabHeader } from './TabHeader';
