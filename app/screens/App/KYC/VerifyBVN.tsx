3e7e99ae9b0e4861e2415583cf9e0b20a7d05f4b
David Bello <davidbello998@gmail.com>
2023-08-16 05:43:08 +0100

    date picker updated

--- app/screens/App/KYC/VerifyBVN.tsx
+++ app/screens/App/KYC/VerifyBVN.tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { Platform } from 'react-native';
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
+
 import {
   ActionText,
   Container,
@@ -10,9 +16,42 @@ import {
 import { KYCRoutes, StackNavigationProps } from 'navigation';
 import { verifyBVNValidationSchema } from 'utils';
 
-export default function VerifyBVN({
-  navigation,
-}: StackNavigationProps<KYCRoutes, 'VerifyBVN'>): JSX.Element {
+type VerifyBVNProps = StackNavigationProps<KYCRoutes, 'VerifyBVN'>;
+
+export default function VerifyBVN({ navigation }: VerifyBVNProps): JSX.Element {
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [dateOfBirth, setDateOfBirth] = useState('');
+
+  const toggleDatePicker = () => {
+    setShowPicker(!showPicker);
+  };
+
+  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    if (event.type === 'set') {
+      const currentDate = selectedDate || date;
+      setDate(currentDate);
+      if (Platform.OS === 'android') {
+        toggleDatePicker();
+        setDateOfBirth(formatDate(currentDate));
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
+
+  const formatDate = (rawDate: Date) => {
+    const fdate = new Date(rawDate);
+    const year = fdate.getFullYear();
+    let month = fdate.getMonth() + 1;
+    let day: number = fdate.getDate();
+
+    month = month < 10 ? Number.parseInt(`0${month}`, 10) : month;
+    day = day < 10 ? Number.parseInt(`0${day}`, 10) : day;
+
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <Container header>
       <Title title="Verify BVN" subtitle="Please fill this form to verify your BVN." />
@@ -29,12 +68,26 @@ export default function VerifyBVN({
           });
         }}>
         <FormField label="BVN" name="bvn" placeholder="Enter BVN" />
+
         <FormField
           rightIcon="calendar"
           label="Date of Birth"
           name="dob"
+          value={dateOfBirth}
           placeholder="dd/mm/yyyy"
+          onRightIconPress={toggleDatePicker}
+          onChangeText={setDateOfBirth}
         />
+
+        {showPicker && (
+          <DateTimePicker
+            mode="date"
+            display="spinner"
+            value={date}
+            onChange={onChange}
+            style={{ zIndex: 1 }}
+          />
+        )}
         <Divider />
         <Submit label="Verify" />
         <Divider />
